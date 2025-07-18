2108fcc145082dbcd8be522caa47633ac45aa129
v.kuzmin <hikkathon@gmail.com>
2025-07-18 09:46:42 +0300

    update

--- src/services/kodik.service.ts
+++ src/services/kodik.service.ts
@@ -3,7 +3,7 @@ import { KodikResponse } from "@/types/kodik.response.ts";
 
 export default class KodikService {
   static async getSerial(shikimoriId: number): Promise<KodikResponse> {
-    const response = await api.get<KodikResponse>(`/search?token=YOU_KODIK_TOKEN&shikimori_id=${shikimoriId}`);
+    const response = await api.get<KodikResponse>(`v1/watch?sid=${shikimoriId}`);
     return response.data;
   }
 }
